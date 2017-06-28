388fe5bdd7fb3ac548d02ba9aa636df52f6cee8d
paspheeris <pa.spheeris@gmail.com>
2017-06-28 16:26:32 -0700

    basic functionality

--- Advanced Front End Development Projects/simon/simon.js
+++ Advanced Front End Development Projects/simon/simon.js
@@ -3,6 +3,11 @@ class Model {
     {
         this.sequence = [];
         this.count = 0;
+
+        this.playerSeq = [];
+        this.playerSeqCount = 0;
+        this.awaiting = false;
+
         this.colors = ['green', 'red', 'yellow', 'blue'];
         this.appState = {
             on: false,
@@ -20,17 +25,46 @@ class Model {
     }
     incrementSequence()
     {
+        console.log('fn: model.incrementSequence');        
         this.count++;
+        // console.log(this.sequence);
         this.sequence.push(this.randomColor);
     }
     emptySequence()
     {
         this.count = 0;
-        this.sequence = 0;
+        this.sequence = [];
     }
     getCountString()
     {
         return this.count.toString();
+    }
+    getPlayerSeq() 
+    {
+        return this.playerSeq;
+    }
+    getPlayerSeqCount() 
+    {
+        return this.playerSeqCount;
+    }
+    emptyPlayerSeq()
+    {
+        this.playerSeqCount = 0;
+        this.playerSeq = [];
+    }
+    getPlayerSeqStatus()
+    {
+        let compLast = this.sequence[this.count - 1];
+        let playerLast = this.playerSeq[this.playerSeqCount - 1];
+        //If the player hasn't made as many moves
+        if(this.getPlayerSeqCount() < this.count) {
+            return 'unresolved';
+        } else if (compLast !== playerLast) {
+            return 'failed';
+        } else if (this.getPlayerSeqCount() === this.count &&
+                    compLast === playerLast) {
+            return 'matched';
+        }
     }
 }
 class Controller {
@@ -43,13 +77,15 @@ class Controller {
     eventDispatcher()
     {
         document.addEventListener('click', e => {
-            console.log(e.target.classList);
+            // console.log(e.target.classList);
             //create array from DOMtokenlist
             const classListing = Array.from(e.target.classList);
             // console.log(classListing);
             if(classListing.includes('on-off')) this.onOffButton(e);            
             if(classListing.includes('start-button')) this.startButton(e);
             if(classListing.includes('strict-button')) this.strictButton(e);
+            if(classListing.includes('color-button')) this.colorClick(e);
+            
         });
     }
     onOffButton()
@@ -61,9 +97,14 @@ class Controller {
     startButton() 
     {
         if(this.model.appState.on === false) return;
+        this.model.emptySequence();
         console.log('fn: controller.startButton()');
 
-        this.view.renderCountDisplay('--', true);
+        this.view.renderCountDisplayAsync('--')
+            .then(() => {
+                this.view.renderCountDisplaySync(this.model.getCountString());
+                this.playSequence();
+            });
         // this.view.renderCountDisplay(this.model.getCountString());
         // this.model.incrementSequence();
         // this.model.getSequence();
@@ -72,6 +113,62 @@ class Controller {
     {
         if(this.model.appState.on === false) return;
         console.log('fn: controller.strictButton()');
+    }
+    playSequence()
+    {
+        console.log('fn: controller.playSequence');
+        if(this.model.count === 0) this.model.incrementSequence();
+
+        this.view.renderSequenceAsync(this.model.getSequence())
+            .then(() => {
+                return this.awaitingtPlayer();
+            })
+            .then((status) => {
+                console.log('resolving promis', status);
+                if(status === 'matched'){
+                    this.model.incrementSequence();
+                    this.playSequence();
+                } else if (status === 'failed') {
+                    this.playSequence();
+                }
+            })
+    }
+    awaitingtPlayer()
+    {
+        console.log('fn: controller.awaitingPlayer');
+        this.model.awaiting = true;
+
+        
+        return new Promise((res, rej) => {
+            const interv = setInterval(() => {
+                let status = this.model.getPlayerSeqStatus();
+                if(status === 'failed') {
+                    console.log('failure');
+                    clearInterval(interv);
+                    this.model.awaitingtPlayer = false;
+                    res(status);
+                } else if (status === 'matched') {
+                    console.log('matched');
+                    clearInterval(interv);             
+                    this.model.awaitingtPlayer = false;                           
+                    res(status);
+                } 
+                // else if (status === 'unresolved') {
+                //     // console.log('unresolved');
+                // }
+            }, 250);
+        });
+    }
+    colorClick(e)
+    {
+        if(this.model.awaiting === false) return;
+
+        let color = Array.from(e.target.classList)[0];
+        console.log('fn: controller.colorClick');
+        // console.log(Array.from(e.target.classList));
+        this.view.animateColorClick(color);
+        this.model.playerSeq.push(color);
+        this.model.playerSeqCount++;
     }
 }
 class View {
@@ -90,23 +187,62 @@ class View {
         this.strictIndicator = document.querySelector('.strict-indicator');
         this.onOff = document.querySelector('.on-off');    
     }
-    renderCountDisplay(string, flash=false) {
-        console.log('fn view.renderCountDisplay()');
+    renderCountDisplayAsync(string) {
+        console.log('fn view.renderCountDisplayAsync()');
         console.log(this.countDisplay.textContent === '');
-        if(flash) {
+        return new Promise((res, rej) => { 
             let i = 0;
-            this.countDisplay.innerText = string;
-            
+            this.countDisplay.innerText = string;  
             const interv = setInterval(() => {
                 i++;
                 this.countDisplay.textContent = this.countDisplay.textContent === string ? 
                     "" :
                     string;
-                if(i >= 4) clearInterval(interv);
+                if(i >= 4) {
+                    clearInterval(interv);
+                    res();
+                }
             }, 500);
-        } else {
-            this.countDisplay.textContent = string;
         }
+    )}
+    renderCountDisplaySync(string) {
+        this.countDisplay.textContent = string;
+    }
+    renderSequenceAsync(arr) {
+        console.log('fn: view.renderSequenceAsync');
+        console.log({arr});
+        //Turn off audio and remove visual effects from all four colors to clear previous
+        let prevColor = '';
+        if(prevColor) {
+            this[prevColor].style.opacity = 1;
+        }
+        
+        return new Promise((res, rej) => {
+            let i = 0;
+            const interv = setInterval(() => {
+                if(i >= arr.length) {
+                    clearInterval(interv);
+                    console.log(this.prevColor);
+                    this[prevColor].style.opacity = 1;
+                    res();
+                } else {
+                    this.animate(arr[i]);
+                    prevColor = arr[i];
+                    i++;
+                }
+        }, 1000);
+        })
+    }
+    animate(color) {
+        this[color].style.opacity = '.5';
+    }
+    animateColorClick(color)
+    {
+        console.log('fn: view.animateAsync');
+        this[color].style.opacity = '.5';
+        setTimeout(() => {
+            this[color].style.opacity = '1';
+        }, 750);
     }
 }
 
