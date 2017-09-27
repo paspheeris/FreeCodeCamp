ea1d1833b67bb2911eb288aef6c14dc4aa58fdff
paspheeris <pa.spheeris@gmail.com>
2017-09-27 13:41:41 -0700

    add new backend

--- Dynamic Web Application Projects/voting-app/back-end/data/polls.js
+++ Dynamic Web Application Projects/voting-app/back-end/data/polls.js
@@ -1,10 +1,5 @@
-const uuidv4 = require('uuid/v4');
-//poll_choices => allChoices
-//poll_votes => votesByChoice
-//poll_question => question
-export const polls = {
-  byId : {
-    "4d4cdb42-3475-432f-bccc-c3cb5d7765d9":  {
+module.exports = [
+    {
       key: "4d4cdb42-3475-432f-bccc-c3cb5d7765d9",
       author_id: "google-oauth2|105786279754106054402",
       question: 'What\'s the best city?' ,
@@ -17,7 +12,7 @@ export const polls = {
       participantsByIp: ['11.111.111.111', '33.333.333.333'],
       participantsById: ["google-oauth2|105786279754106054402"]
     },
-    "e4c25936-55d8-44a8-8684-eb0bc7720442":  {
+    {
       key: "e4c25936-55d8-44a8-8684-eb0bc7720442",
       author_id: "google-oauth2|105786279754106054402",
       question: 'What\'s the best color',
@@ -31,7 +26,7 @@ export const polls = {
       participantsByIp: ['11.111.111.111', '33.333.333.333'],
       participantsById: ["google-oauth2|105786279754106054402"]
     },
-    "27fb4dab-c512-4685-a6a4-2003a8276439": {
+    {
       key: "27fb4dab-c512-4685-a6a4-2003a8276439",
       author_id: "github|20876393",
       question: 'What\'s your favorite food?',
@@ -45,7 +40,7 @@ export const polls = {
       participantsByIp: ['22.222.222.222', '33.333.333.333'],
       participantsById: ["github|20876393"]
     },
-    "f66e9aea-0d84-438c-bffe-d7fec535929b": {
+    {
       key: "f66e9aea-0d84-438c-bffe-d7fec535929b",
       author_id: "github|20876393",
       question: 'What\'s your favorite sport?',
@@ -59,36 +54,5 @@ export const polls = {
       participantsByIp: ['22.222.222.222', '33.333.333.333'],
       participantsById: ["github|20876393"]
     },
-  },
-  allIds : ["4d4cdb42-3475-432f-bccc-c3cb5d7765d9", "e4c25936-55d8-44a8-8684-eb0bc7720442", "27fb4dab-c512-4685-a6a4-2003a8276439", "f66e9aea-0d84-438c-bffe-d7fec535929b"]
-};
-
-export const users = {
-  byId: {
-    "ae650b55-e8b0-4138-97ba-86493d7c4f2d": {
-      key: "ae650b55-e8b0-4138-97ba-86493d7c4f2d",
-      name: 'Paul',
-      password: 'Password1',
-      polls: [],
-    },
-    "bc466e9d-ec0a-48fd-ac98-89bbbfe5fd58": {
-      key: "bc466e9d-ec0a-48fd-ac98-89bbbfe5fd58",
-      name: 'Brandon',
-      password: 'Password2',    
-      polls: [],
-    },
-    "ba5844f0-c25e-4970-b83c-be71321122e2": {
-      key: "ba5844f0-c25e-4970-b83c-be71321122e2",
-      name: 'Will',
-      password: 'Password3',  
-      polls: [],
-    },
-    "00857e1a-c73f-437b-87e9-733417e12f0f": {
-      key: "00857e1a-c73f-437b-87e9-733417e12f0f",
-      name: 'Wyatt',
-      password: 'Password4',    
-      polls: [],
-    }
-  },
-  allIds: ["ae650b55-e8b0-4138-97ba-86493d7c4f2d", "bc466e9d-ec0a-48fd-ac98-89bbbfe5fd58", "ba5844f0-c25e-4970-b83c-be71321122e2", "00857e1a-c73f-437b-87e9-733417e12f0f"]
-};
+];
+// export default JSON.stringify(polls);
---
+++ Dynamic Web Application Projects/voting-app/back-end/routes/index.js
@@ -0,0 +1,100 @@
+var express = require('express');
+var router = express.Router();
+
+// const mongoose = require('mongoose');
+// mongoose.connect(process.env.DATABASE);
+// mongoose.Promise = global.Promise;
+// const Poll = mongoose.model('Poll');
+const Poll = require('../models/Poll');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+router.get('/polls', (req, res) => {
+  Poll.find({}).exec()
+    .then(data => {
+      // console.log(data)
+      res.json(data);
+    })
+    .catch(error => {
+      console.log(error)
+      res.json({error})
+    })
+});
+router.patch('/poll/vote/:_id', (req, res) => {
+  console.log(req.body);
+
+  if(req.body.addedChoice) {
+    Poll.findOne({_id: req.params._id})
+      // .select('votesByChoice allChoices')
+      .then(something => {
+        // console.log(something);
+        something.votesByChoice.push({choiceName: req.body.choice, count: 1});
+        something.allChoices.push(req.body.choice);
+        // console.log(something);        
+        return something.save()
+      })
+      .then(saved => {
+        res.json(saved);
+        // console.log(saved);
+      })
+      .catch(error => {
+        console.log(error);
+      })
+      return;
+  }
+   const poll = Poll.findOneAndUpdate(
+     { _id: req.params._id, 'votesByChoice.choiceName': req.body.choice}, 
+    { $inc: {'votesByChoice.$.count': 1}}, {
+    new: true, //return the new Poll instead of the old one
+    runValidators: true
+  }).exec()
+    .then(something => {
+      // console.log(something);
+      res.json(something);
+      // res.json(JSON.stringify(something));
+    })
+    .catch(error => res.json({error}));
+    // poll.then(some => console.log(some));
+    // res.send('ol');
+    // res.json(JSON.stringify(poll));
+});
+router.post('/poll/create', (req, res) => {
+  // console.log(req.body.poll);
+  const poll = new Poll(req.body.poll);
+  // console.log(poll);
+  poll.save()
+    .then(saved => {
+      console.log(saved);
+      res.json(saved);
+    })
+    .catch(error => {
+      console.log(error)
+      res.json({error});
+    });
+});
+router.patch('/poll/edit/:_id', (req, res) => {
+  console.log(req.params._id);
+  const newPoll = new Poll(req.body.poll);
+
+  Poll.findByIdAndUpdate({_id: req.params._id}, newPoll, {new: true}).exec()
+    .then(updatedPoll => {
+      console.log(updatedPoll);
+      res.json(updatedPoll);
+    })
+    .catch(error => {
+      console.log(error);
+      res.json({error});
+    })
+  // const poll = Poll.find({_id: req.params._id}).exec();
+  // poll.then(something => {
+  //   console.log(something);
+  // })
+  //   .catch(error => {
+  //     console.log(error);
+  //     res.json({error});
+  //   })
+});
+
+module.exports = router;
