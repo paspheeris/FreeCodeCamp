2e212deacc0d4803030cabbddd21245918daf8a8
paspheeris <pa.spheeris@gmail.com>
2017-09-27 20:18:52 -0700

    landingpage css work

--- Dynamic Web Application Projects/voting-app/front-end/src/components/LandingPage.js
+++ Dynamic Web Application Projects/voting-app/front-end/src/components/LandingPage.js
@@ -1,21 +1,31 @@
 import React from 'react';
-import FetchDataButton from './FetchDataButton';
 import auth from '../auth/Auth';
+import { Segment, Icon, Divider } from 'semantic-ui-react';
 
 // const auth = new Auth();
 
 const LandingPage = () => {
   return (
-    <div>
-      <p>Hey, LandingPage</p>
-      <FetchDataButton />
-      <button onClick={() => auth.login()} value="clickme">Login</button>
-      <button onClick={() => auth.handleAuthentication()} value="clickme">handleAuthentication</button>
-      <button onClick={() => auth.logout()} value="clickme">Logout</button>
+    <div className="LandingPage-wrapper">
+      <h1 className="pollsDisplay-topText">FreeCodeCamp Polling App</h1>
+      <Divider horizontal />
+      <div className="LandingPage-lowerSegment">
+        <div className="LandingPage-Segment">
+          <ul>
+            <li>Create and share polls with your friends</li>
+            <li>Create and share polls with your friends</li>
+            <li>Create and share polls with your friends</li>
+            <li>Create and share polls with your friends</li>
+            <li>Create and share polls with your friends</li>
+            <li>Create and share polls with your friends</li>
+            <li>Create and share polls with your friends</li>
 
-      <button onClick={() => console.log(auth.isAuthenticated())} value="clickme">isAuthenticated</button>
+          </ul>
+        </div >
+        <img className="ballotBox-image" src='ballotBox.svg' alt="<div>Icons made by <a href='http://www.freepik.com' title='Freepik'>Freepik</a> from <a href='https://www.flaticon.com/' title='Flaticon'>www.flaticon.com</a> is licensed by <a href='http://creativecommons.org/licenses/by/3.0/' title='Creative Commons BY 3.0' target='_blank'>CC 3.0 BY</a></div>'" />
+        {/* <Divider vertical /> */}
+      </div>
 
-      
     </div>
   )
 }
