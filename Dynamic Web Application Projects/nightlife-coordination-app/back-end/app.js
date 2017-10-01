efa8bf817ee48492dec3e3aeeaa20e3e02fc0b73
paspheeris <pa.spheeris@gmail.com>
2017-10-01 16:06:54 -0700

    trying to deploy

--- Dynamic Web Application Projects/nightlife-coordination-app/back-end/app.js
+++ Dynamic Web Application Projects/nightlife-coordination-app/back-end/app.js
@@ -10,10 +10,9 @@ var users = require('./routes/users');
 const cors = require('cors');
 const mongoose = require('mongoose');
 var app = express();
-if(process.env.NODE_ENV === 'development') {
-  console.log('in cors if');
-  app.use(cors());
-}
+// if (process.env.NODE_ENV === 'development') {
+app.use(cors());
+// }
 require('dotenv').config();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -26,25 +25,27 @@ mongoose.connection.on('error', (err) => {
 });
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(express.static(path.join(__dirname, 'build')));
 
 app.use('/', index);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
--- Dynamic Web Application Projects/nightlife-coordination-app/back-end/routes/index.js
+++ Dynamic Web Application Projects/nightlife-coordination-app/back-end/routes/index.js
@@ -4,46 +4,33 @@ var fetch = require('node-fetch');
 
 var Yelp = require('yelpv3');
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
-router.get('/yelp/bars/:location', (req, res) => {
+router.get('/api/yelp/bars/:location', (req, res) => {
   let id = process.env.YELP_CLIENT_ID;
   let secret = process.env.YELP_CLIENT_SECRET;
   let access_token = undefined;
-  // console.log('in route');
   fetch(`https://api.yelp.com/oauth2/token?grant_type=client_credentials&client_id=${id}&client_secret=${secret}`,
-  {method: 'POST'})
+    { method: 'POST' })
 
     .then(data => {
       return data.json();
     })
     .then(data => {
-      // console.log(data);
-      // console.log(data.access_token);
       access_token = data.access_token;
-      console.log(req.params.location);
       return fetch(`https://api.yelp.com/v3/businesses/search?term=bar&location=${req.params.location}`,
-      {
-        headers: {
-          Authorization: `Bearer ${access_token}`
-        }
-      })
- 
+        {
+          headers: {
+            Authorization: `Bearer ${access_token}`
+          }
+        })
+
     })
     .then(data => {
       return data.json();
     })
     .then(json => {
-      // console.log(json);
-    //   var fs = require('fs');
-    //   fs.writeFileSync("./test", JSON.stringify(json), function(err) {
-    //   if(err) {
-    //       return console.log(err);
-    //   }
-
-    // console.log("The file was saved!");
-// }); 
       res.json(json);
     })
     .catch(error => {
